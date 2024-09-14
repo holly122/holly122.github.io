4f02df1ff9cc500e3b1ab31167204dbfc3848c5e
holly122 <hollyyclapp@gmail.com>
2024-09-14 21:59:07 +0100

    changes

---
+++ app.js
@@ -0,0 +1,9 @@
+Document.addeventlistener("DOMContentLoaded", () => {
+    
+    const images = document.querySelectorAll("img") ; 
+
+    for(image of images){
+        fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json()).then(data => image.src = data.message)
+    }
+
+}
